23bd69d9bfc9ee19b680935aa65242d9faf5f340
d0artG <149911313+d0artG@users.noreply.github.com>
2024-08-06 18:11:54 +0100

    Update time_series_visualizer.py

--- time_series_visualizer.py
+++ time_series_visualizer.py
@@ -25,17 +25,38 @@ def draw_line_plot():
 
 def draw_bar_plot():
     # Copy and modify data for monthly bar plot
-    df_bar = None
+    df_bar=df
+    df_bar["year"] = df_bar.index.year
+    df_bar["month"] = df_bar.index.month_name()
+    df_bar=df_bar.groupby(["year","month"]).mean().reset_index()
 
     # Draw bar plot
+    fig,ax=plt.subplots()
+    ax=sns.barplot(data=df_bar, x="year",y="views", hue="month", hue_order=["January", "February", "March", "April","May", "June", "July", "August", "September", "October", "November", "December"])
+    ax.set(xlabel='Years', ylabel='Average Page Views')
+
+
+    # Save image and return fig (don't change this part)
+    fig.savefig('bar_plot.png')
+    return fig
+
+def draw_box_plot():
+    # Prepare data for box plots (this part is done!)
+    df_box = df.copy()
+    df_box.reset_index(inplace=True)
+    df_box['year'] = [d.year for d in df_box.date]
+    df_box['month'] = [d.strftime('%b') for d in df_box.date]
+
+    # Draw box plots (using Seaborn)
 
 
 
 
 
     # Save image and return fig (don't change this part)
-    fig.savefig('bar_plot.png')
+    fig.savefig('box_plot.png')
     return fig
+
 
 def draw_box_plot():
     # Prepare data for box plots (this part is done!)
